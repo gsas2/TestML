2af501a75fa0419e9c8e448977d3732b2f028ff1
Gaston Sas <gaston@oasiscollections.com>
2018-06-16 16:27:32 -0300

    Frontend - List Page - Styling complete

--- src/pages/ListItemsPage/ListItem/ListItem.jsx
+++ src/pages/ListItemsPage/ListItem/ListItem.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ListItem.css';
 import FreeShippingIcon from './ic_shipping.png';
 import FreeShippingIconX2 from './ic_shipping@2x.png.png';
 
-const ListItem = () => (
+const ListItem = ({product}) => (
   <div className="listItem">
     <div className="imageContainer">
-      <img src="https://placeimg.com/180/180/any" alt="Product" />
+      <img src={product.picture} alt="Product" />
     </div>
     <div className="detailsContainer">
       <div className="price">
-        <span>$ 1.980</span>
-        <img className="freeShipping"
-              src={FreeShippingIcon}
-              srcSet={[`${FreeShippingIcon} 1x`, `${FreeShippingIconX2} 2x`]}
-              alt="Envío Gratis" />
+        {product.price.currency} {new Intl.NumberFormat().format(product.price.amount)} {product.price.decimals || ''}
+        { product.free_shipping && 
+          <img className="freeShipping"
+            src={FreeShippingIcon}
+            srcSet={[`${FreeShippingIcon} 1x`, `${FreeShippingIconX2} 2x`]}
+            alt="Envío Gratis" />
+        }
       </div>
-      <div className="description">Apple Ipod Touch 5g 16gb Negro Igual A Nuevo Completo Unico!</div>
+      <div className="title">{product.title}</div>
     </div>
     <div></div>
     <div className="locationContainer">
-      Capital Federal
+      {product.location}
     </div>
   </div>
 );
+
+ListItem.propTypes = {
+  product: PropTypes.object.isRequired
+};
 
 export default ListItem;
--- src/pages/ListItemsPage/ListItemsPage.jsx
+++ src/pages/ListItemsPage/ListItemsPage.jsx
@@ -1,11 +1,75 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
+import './ListItemsPage.css';
+
+const products = [
+  {
+      "id": "MLA666799963",
+      "title": "Ipod Classic",
+      "price": {
+          "currency": "$",
+          "amount": 1300,
+          "decimals": 0
+      },
+      "picture": "http://mla-s2-p.mlstatic.com/928485-MLA25600636874_052017-I.jpg",
+      "condition": "new",
+      "free_shipping": true,
+      "location": "Capital Federal"
+  },
+  {
+      "id": "MLA717988441",
+      "title": "Ipod 7ma Generación 16 Gb",
+      "price": {
+          "currency": "$",
+          "amount": 850,
+          "decimals": 0
+      },
+      "picture": "http://mla-s2-p.mlstatic.com/601625-MLA26587913799_012018-I.jpg",
+      "condition": "used",
+      "free_shipping": false,
+      "location": "Capital Federal"
+  },
+  {
+      "id": "MLA697459513",
+      "title": "Ipod Touch 4ta Generacion 32 Gb",
+      "price": {
+          "currency": "$",
+          "amount": 680,
+          "decimals": 0
+      },
+      "picture": "http://mla-s2-p.mlstatic.com/738532-MLA25881108346_082017-I.jpg",
+      "condition": "used",
+      "free_shipping": false,
+      "location": "Capital Federal"
+  },
+  {
+      "id": "MLA711127459",
+      "title": "Ipod Touch 6 Último Modelo Gold 32gb Permuto",
+      "price": {
+          "currency": "$",
+          "amount": 7900,
+          "decimals": 0
+      },
+      "picture": "http://mla-s2-p.mlstatic.com/977352-MLA26953209493_032018-I.jpg",
+      "condition": "used",
+      "free_shipping": false,
+      "location": "Buenos Aires"
+  }
+];
 
 const ListItemsPage = () => (
-  <div>
-    LIST ITEMS PAGE!
-    <ListItem />
-    <ListItem />
+  <div className="listItemsPage">
+    <div className="breadcrumbContainer">
+      Lorem > Ipsum > ... > IPod
+    </div>
+    <div className="resultsContainer">
+      { products.map(product => (
+          <div className="resultContainer">
+            <ListItem product={product} />
+            <div className="itemSeparator" />
+          </div>
+        )) }
+    </div>
   </div>
 );
 
